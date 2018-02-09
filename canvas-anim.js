12eac628fb242ec88f2fd16c3cde72a60a8fc067
srafi1 <shakilrafi0@gmail.com>
2018-02-08 21:24:06 -0500

    update

--- canvas-anim.js
+++ canvas-anim.js
@@ -1,4 +1,4 @@
-var c, ctx, size, grow, animationId, running;
+var c, ctx, size, grow, x, y, dx, dy, animationId, running, whichAnimation;
 
 function main() {
     c = document.getElementById("theCanvas");
@@ -7,21 +7,28 @@ function main() {
 
     size = 40;
     grow = 0.5;
+    x = 290;
+    y = 300;
+    dx = Math.random()*2 + 1;
+    dy = Math.random()*2 + 1;
     running = false;
+    whichAnimation = 0;
 
     drawCircle();
 }
 
-function startAnim() {
+function startAnim1() {
+    if (running && whichAnimation == 1) {
+        stopAnim();
+    }
     if (!running) {
-        anim();
+        whichAnimation = 0;
+        anim1();
         running = true;
     }
 }
 
-function anim() {
-    running = true;
-
+function anim1() {
     size += grow;
     if (size < 20) {
         grow = 0.5;
@@ -32,13 +39,51 @@ function anim() {
     ctx.clearRect(0, 0, 600, 600);
     drawCircle();
 
-    animationId = window.requestAnimationFrame(anim);
+    animationId = window.requestAnimationFrame(anim1);
     //console.log(animationId)
+}
+
+function startAnim2() {
+    if (running && whichAnimation == 0) {
+        stopAnim();
+    }
+    if (!running) {
+        whichAnimation = 1;
+        anim2();
+        running = true;
+    }
+}
+
+function anim2() {
+    x += dx;
+    y += dy;
+    
+    if (x - size < 0 || x + size > 600) {
+        dx *= -1;
+        if (x - size < 0) {
+            x = size;
+        } else {
+            x = 600 - size;
+        }
+    }
+    if (y - size < 0 || y + size > 600) {
+        dy *= -1;
+        if (y - size < 0) {
+            y = size;
+        } else {
+            y = 600 - size;
+        }
+    }
+
+    ctx.clearRect(0, 0, 600, 600);
+    drawCircle();
+    
+    animationId = window.requestAnimationFrame(anim2);
 }
 
 function drawCircle() {
     ctx.beginPath();
-    ctx.arc(300, 300, size, 0, Math.PI * 2);
+    ctx.arc(x, y, size, 0, Math.PI * 2);
     ctx.fill();
 }
 
